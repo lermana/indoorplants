d302b26a111b77a4584acadfec2143ce0acdad6f
Abe Lerman <abe893@gmail.com>
2017-04-03 23:43:41 -0400

    first push

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # modeling
-An abstraction of my modeling workflow. 
+
+This is a Python library of helper functions and workflow automaters that I use to make my life easier, the first installment of which is **evaluation**.
+
+### evaluation
+
+A module of functions that allow for streamlined and tidy model evaluation, for use with models from *sci-kit-learn*.
+
+This is a first pass; the code works, but has not been optimized. Additionally, some changes could be made here for style and clarity.
+
+Some key functions are:
+
+**five_fold_validation**: allows for automated model evaluation. 
+
+**validate_multiple_models**: runs *five_fold_model_validation* for multiple models, returning aggregate results. 
+
+More information on these functions can be found on their respective pages on the wiki. 
+
+Additional functions include decomposed routines that are used by the aforementioned validation functions, as well as some wrappers for sk-learn's *confusion_matrix* that can be used for evaluating the performance of classifiers.
+    
+This code was written using *Python 3.5*, using *Pandas 0.19.2*, *sci-kit learn 0.18.1*, and *NumPy 1.12.0*, I can't think of anything here that requires Pandas .19, and ditto for NumPy 1.12. If using an older version of sci-kit learn however, the confusion_matrix function will have to be imported from *metrics* and not *model_selection*.	
---
+++ evaluation.py
@@ -0,0 +1,128 @@
+import pandas
+import numpy
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import confusion_matrix
+
+
+
+def split_data(y, n_splits=5, random_state=0):
+    """input: variable to be predicted, [n_splits, random_state].
+    returns: shuffled StratifiedKFold - iterator of n 
+    train_index, test_index pairs."""
+    skf = StratifiedKFold(n_splits=n_splits, 
+                          shuffle=True, 
+                          random_state=random_state)
+    return skf.split(numpy.zeros(len(y)),
+                     y)
+
+
+def train_and_test(model, X_train, y_train, X_test, y_test):
+    """input: instantiated model, training & test data sets.
+    returns: training accuracy, test accuracy."""
+    model = model.fit(X_train, 
+                      y_train)
+
+    return model.score(X_train, 
+                       y_train), model.score(X_test, 
+                                                y_test)
+
+
+def get_model_variants(model, params_list):
+    """input: uninstantiated model object, list of model param dictionaries.
+    returns: generator of instantiated (model(params), params) tuples."""
+    return ((model(**params), params) for params in params_list)
+    
+
+def get_models(model, params_list):
+    """wrapper for get_model_variants.
+    same inputs but handles exceptions due to no params_list,
+    in which case a tuple (model(), 'Default') is returned."""
+    try:
+        return get_model_variants(model, params_list)
+    except TypeError:
+        return [(model(), 'Default')]
+
+
+def five_fold_validation(model_func, X, y, params_list=None):
+    """input: uninstantiated model object, X, y, [params_list].
+    returns: DataFrame of test and train accuracies for each of
+    5 shuffled stratified trials, for each set of 
+    model parameters."""
+    results = []
+    for fold, (train_index, test_index) in enumerate(split_data(y), 1):
+        for model, params in get_models(model_func, params_list):
+            train_acc, \
+            test_acc = train_and_test(model, 
+                                      X.iloc[train_index], 
+                                      y.iloc[train_index],
+                                      X.iloc[test_index],
+                                      y.iloc[test_index])
+            try:
+                params_str = ', '.join('{}: {}'.format(param, 
+                									   val) 
+                                        for param, val in params.items())
+            except AttributeError:
+                params_str = params
+            results.append((fold,
+            	            params_str,
+            	            train_acc,
+            	            test_acc))
+    cols = ['trial',
+    		'params',
+    		'train_accuracy',
+    		'test_accuracy']
+    return pandas.DataFrame(results,
+                             columns=cols).set_index(['trial', 
+                                                      'params'],
+                                                     drop=True)
+
+
+def validate_multiple_models(model_funcs, X, y, params_list=None):
+    """input: same as five_fold_validation, except model_funcs 
+    must be an iterator. Note that all models will run off the same
+    params_list.
+    returns: concatenated results of five_fold_validation."""
+    results = []
+    for model_func in model_funcs:
+        model_name = str(model_func).split()[1].split('.')[3][:-2]
+        results.append(pandas.concat({model_name:
+                                      five_fold_validation(model_func, 
+                                                           X, 
+                                                           y, 
+                                                           params_list)},
+                                      axis=1))
+    return pandas.concat(results, axis=1)
+
+
+def get_confusion(y_test, y_hat, index):
+    """returns sklearn confusion matrix as DataFrame,
+    with index serving as labels for rows and columns."""
+    return pandas.DataFrame(confusion_matrix(y_test, 
+                                             y_hat),
+                            index=index,
+                            columns=index)
+
+
+def get_precision(y_test, y_hat, index):
+    """returns by-value precision as DataFrame."""
+    return pandas.DataFrame(
+                numpy.diag(
+                    confusion_matrix(y_test,
+                                     y_hat)
+                         / confusion_matrix(y_test,
+                                            y_hat).sum(axis=0)
+                 ), index=index,
+                    columns=['Precision'])
+
+
+def get_recall(y_test, y_hat, index):
+    """returns by-value recall as DataFrame."""
+    return pandas.DataFrame(
+                numpy.diag(
+                    confusion_matrix(y_test,
+                                     y_hat)
+                         / confusion_matrix(y_test,
+                                            y_hat).sum(axis=1)
+                 ), index=index,
+                    columns=['Recall'])
