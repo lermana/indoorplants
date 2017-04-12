cc7944162dd5d1d6558ce78129e622aed8031026
Abe Lerman <abe893@gmail.com>
2017-04-12 18:31:30 -0400

    refactored evaulation

--- evaluation.py
+++ evaluation.py
@@ -1,88 +1,153 @@
 import pandas
 import numpy
 
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import StratifiedKFold, \
+                                    KFold
+
+from sklearn.metrics import confusion_matrix, \
+                            accuracy_score, \
+                            precision_score, \
+                            recall_score, \
+                            f1_score
 
 
 
-def split_data(y, n_splits=5, random_state=0):
-    """input: variable to be predicted, [n_splits, random_state].
-    returns: shuffled StratifiedKFold - iterator of n 
-    train_index, test_index pairs."""
-    skf = StratifiedKFold(n_splits=n_splits, 
-                          shuffle=True, 
-                          random_state=random_state)
-    return skf.split(numpy.zeros(len(y)),
-                     y)
+def get_function(func_clf, func_reg, problem):
+    try:
+        return {'clf': func_clf, 'reg': func_reg}[problem]
+    except KeyError:
+        raise KeyError("Must pass either 'clf' or 'reg'.")
 
 
-def train_and_test(model, X_train, y_train, X_test, y_test):
-    """input: instantiated model, training & test data sets.
-    returns: training accuracy, test accuracy."""
-    model = model.fit(X_train, 
-                      y_train)
+def generate_folds(fold_obj, y, n_splits=5, random_state=0):
+    return fold_obj(n_splits=n_splits, 
+                    shuffle=True, 
+                    random_state=random_state).split(numpy.zeros(len(y)),
+                                                     y)
 
-    return model.score(X_train, 
-                       y_train), model.score(X_test, 
-                                                y_test)
+
+def split_for_clf(y):
+    return generate_folds(StratifiedKFold, y)
+
+
+def split_for_reg(y):
+    return generate_folds(KFold, y)
+
+
+def get_split_func(problem):
+    return get_function(split_for_clf, split_for_reg, problem)
+
+
+def split_data(y, problem):
+    return get_split_func(problem)(y)
+
+
+def score_reg(model, X_train, y_train, X_test, y_test):
+    return (model.score(X_train, y_train), 
+            model.score(X_test, y_test))
+
+
+def score_clf(model, X_train, y_train, X_test, y_test):
+    y_hat_train = model.predict(X_train)
+    y_hat_test = model.predict(X_test)
+    return (accuracy_score(y_train, 
+                           y_hat_train),
+            accuracy_score(y_test, 
+                           y_hat_test),  
+            precision_score(y_test, 
+                            y_hat_test),
+            recall_score(y_test, 
+                         y_hat_test),
+            f1_score(y_test, 
+                     y_hat_test))
+
+
+def get_score_func(problem):
+    return get_function(score_clf, score_reg, problem)
+
+
+def score_model(model, X_train, y_train, X_test, y_test, problem):
+    return get_score_func(problem)(model, X_train, y_train, X_test, y_test)
+
+
+def train_and_test(model, X_train, y_train, X_test, y_test, problem):
+    model = model.fit(X_train, y_train)
+    return score_model(model, 
+                       X_train, 
+                       y_train, 
+                       X_test, 
+                       y_test, 
+                       problem)
 
 
 def get_model_variants(model, params_list):
-    """input: uninstantiated model object, list of model param dictionaries.
-    returns: generator of instantiated (model(params), params) tuples."""
     return ((model(**params), params) for params in params_list)
     
 
 def get_models(model, params_list):
-    """wrapper for get_model_variants.
-    same inputs but handles exceptions due to no params_list,
-    in which case a tuple of (model(), 'Default') is returned."""
     try:
         return get_model_variants(model, params_list)
     except TypeError:
         return ((model(), 'Default'),)
 
 
-def five_fold_validation(model_func, X, y, params_list=None):
-    """input: uninstantiated model object, X, y, [params_list].
-    returns: DataFrame of test and train accuracies for each of
-    5 shuffled stratified trials, for each set of 
-    model parameters."""
-    results = []
-    for fold, (train_index, test_index) in enumerate(split_data(y), 1):
-        for model, params in get_models(model_func, params_list):
-            train_acc, \
-            test_acc = train_and_test(model, 
-                                      X.iloc[train_index], 
-                                      y.iloc[train_index],
-                                      X.iloc[test_index],
-                                      y.iloc[test_index])
-            try:
-                params_str = ', '.join('{}: {}'.format(param, 
-                									   val) 
-                                        for param, val in params.items())
-            except AttributeError:
-                params_str = params
-            results.append((fold,
-            	            params_str,
-            	            train_acc,
-            	            test_acc))
-    cols = ['trial',
-    		'params',
-    		'train_accuracy',
-    		'test_accuracy']
+def results_dataframe(results, cols):
     return pandas.DataFrame(results,
-                             columns=cols).set_index(['trial', 
-                                                      'params'],
-                                                     drop=True)
+                            columns=cols).set_index(['trial', 
+                                                     'params'],
+                                                    drop=True)
+
+
+def format_clf_validation(results):
+    cols = ['trial',
+            'params',
+            'train_accuracy',
+            'test_accuracy',
+            'test_precision',
+            'test_recall',
+            'test_f1score']
+    return results_dataframe(results, cols)
+
+
+def format_reg_validation(results):
+    cols = ['trial',
+            'params',
+            'train_rsquared',
+            'test_rsquared']
+    return results_dataframe(results, cols)
+
+
+def get_format(problem):
+    return get_function(format_clf_validation, format_reg_validation, problem)
+
+def format_validation(results, problem):
+    return get_format(problem)(results)
+
+
+def five_fold_validation(model_func, X, y, problem, params_list=None):
+    results = []
+    for fold, (train_index, test_index) in enumerate(split_data(y, problem), 1):
+        for model, params in get_models(model_func, params_list):
+            itersults = []
+            itersults.append(fold)
+            try:
+                itersults.append(', '.join('{}: {}'.format(param, 
+                                                           val) 
+                                        for param, val in params.items()))
+            except AttributeError:
+                itersults.append(params)
+            for score in train_and_test(model, 
+                                        X.iloc[train_index], 
+                                        y.iloc[train_index], 
+                                        X.iloc[test_index], 
+                                        y.iloc[test_index], 
+                                        problem):
+                itersults.append(score)
+            results.append(itersults)
+    return format_validation(results, problem)
 
 
 def validate_multiple_models(model_funcs, X, y, params_list=None):
-    """input: same as five_fold_validation, except model_funcs 
-    must be an iterator. Note that all models will run off the same
-    params_list.
-    returns: concatenated results of five_fold_validation."""
     results = []
     for model_func in model_funcs:
         model_name = str(model_func).split()[1].split('.')[3][:-2]
@@ -96,8 +161,6 @@ def validate_multiple_models(model_funcs, X, y, params_list=None):
 
 
 def get_confusion(y_test, y_hat, index):
-    """returns sklearn confusion matrix as DataFrame,
-    with index serving as labels for rows and columns."""
     return pandas.DataFrame(confusion_matrix(y_test, 
                                              y_hat),
                             index=index,
@@ -105,7 +168,6 @@ def get_confusion(y_test, y_hat, index):
 
 
 def get_precision(y_test, y_hat, index):
-    """returns by-value precision as DataFrame."""
     return pandas.DataFrame(
                 numpy.diag(
                     confusion_matrix(y_test,
@@ -117,7 +179,6 @@ def get_precision(y_test, y_hat, index):
 
 
 def get_recall(y_test, y_hat, index):
-    """returns by-value recall as DataFrame."""
     return pandas.DataFrame(
                 numpy.diag(
                     confusion_matrix(y_test,
