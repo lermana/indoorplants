d62a8e617139de1455994ae9bd1a813b90d71f87
Abe Lerman <abe893@gmail.com>
2017-08-14 13:40:07 -0400

    curves is getting messy but functionality now works across balanced subsamples

--- curves.py
+++ curves.py
@@ -47,8 +47,8 @@ def _plot_learning(func):
 
 
 @_plot_learning
-def learning_curve(model_func, model_params=None, score=None, cv=5,
-                   X=None, y=None, bal_inds=None):
+def learning_curve(model_func, model_params=None, score=None, 
+                   X=None, y=None, bal_inds=None, cv=5):
     """*still debugging*: function may have issues.
 
     helper func that wraps sklearn's learning curve function.
@@ -59,15 +59,19 @@ def learning_curve(model_func, model_params=None, score=None, cv=5,
         model, X, y,cv=cv, scoring=score)
     results = {}
 
-    if model_params is not None: model = model_func(**model_params)
-    else: model = model_func()
+    if model_params is not None: 
+        model = model_func(**model_params)
+    else: 
+        model = model_func()
     
     if bal_inds is not None:
-        X_, y_ = X[X.index.isin(bal_inds[0])], y[y.index.isin(bal_inds[0])]
+        X_, y_ = X[X.index.isin(bal_inds[0])
+                    ], y[y.index.isin(bal_inds[0])]
         train_sizes, train_scores, test_scores = get_results(X_, y_)
         i, num = 1, len(bal_inds)
         while i < num:
-            X_, y_ = X[X.index.isin(bal_inds[i])], y[y.index.isin(bal_inds[i])]
+            X_, y_ = X[X.index.isin(bal_inds[i])
+                    ], y[y.index.isin(bal_inds[i])]
             _, train_, test_ = get_results(X_, y_)
             train_scores = np.append(train_scores, train_, axis=1)
             test_scores = np.append(test_scores, test_, axis=1)
@@ -83,25 +87,36 @@ def learning_curve(model_func, model_params=None, score=None, cv=5,
     return results
     
 
+def validation_curve(model_func, X, y, param_name, param_range, 
+                     cv=5, score=None, semilog=False, 
+                     other_params=None, bal_inds=None):
+    """wraps skl validation curve; provides functionality
+    for class-balanced analysis"""
+    get_results = lambda X, y: skl.validation_curve(model, 
+                X, y, param_name=param_name, cv=cv,
+                param_range=param_range, scoring=score)
+    results = {}
 
-def validation_curve(model_func, X, y, param_name, 
-                     param_range, cv=5, score=None,
-                     semilog=False, other_params=None):
-    """wraps sklearn's validation curve"""
-    if other_params is not None:
+    if other_params is not None: 
         model = model_func(**other_params)
-    else:
+    else: 
         model = model_func()
-
-    train_scores, test_scores = \
-                skl.validation_curve(model, 
-                                     X, 
-                                     y,
-                                     param_name=param_name, 
-                                     param_range=param_range,
-                                     cv=cv, 
-                                     scoring=score)
     
+    if bal_inds is not None:
+        X_, y_ = X[X.index.isin(bal_inds[0])
+                    ], y[y.index.isin(bal_inds[0])]
+        train_scores, test_scores = get_results(X_, y_)
+        i, num = 1, len(bal_inds)
+        while i < num:
+            X_, y_ = X[X.index.isin(bal_inds[i])
+                        ], y[y.index.isin(bal_inds[i])]
+            train_, test_ = get_results(X_, y_)
+            train_scores = np.append(train_scores, train_, axis=1)
+            test_scores = np.append(test_scores, test_, axis=1)
+            i += 1
+    else:
+        train_scores, test_scores = get_results(X, y)
+
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
@@ -109,8 +124,7 @@ def validation_curve(model_func, X, y, param_name,
     
     plt.figure(figsize=(11, 8))
     title = plt.title('Validation Curve: {}, across {}'.format(
-                      model_func.__name__,
-                      param_name))
+                      model_func.__name__, param_name))
 
     xlab = plt.xlabel(param_name)
     ylab = plt.ylabel(score)
