6734fc907d48c46027dc04c15fe8d70184068df5
Abe Lerman <abe893@gmail.com>
2017-12-03 15:54:03 -0500

    repo reorg; allow for proper installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='indoorplants',
+      version='1.0',
+      description='tools for data analysis and model validation',
+      author='Abe Lerman',
+      url='https://github.com/lermana/indoorplants',
+      packages=['indoorplants'],
+     )
