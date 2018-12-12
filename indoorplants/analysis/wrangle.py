23094f11401ea927dee72496571aca368294b50e
abe <alerman@contently.com>
2018-12-11 20:13:13 -0500

    adding new module, exploratory.wrangle

---
+++ indoorplants/analysis/wrangle.py
@@ -0,0 +1,71 @@
+def get_binary_feature_size_by_class(df, class_, feature):
+    """
+    Given DataFrame, class column name, and feature column
+    name, return what is I think, literally:
+	- pd.crosstab(df.class_, df.feature).stack()
+    Should this be a function then? Probably not. But
+    hey, here it is.
+
+    Parameters
+    ----------
+    
+    df : pandas.DataFrame
+        DataFrame on which this function will operate.
+
+    class_ : str
+	Column name for the class / target.
+
+    feature : str
+	Column name for the feature.
+    
+
+    Return
+    ------
+
+    DataFrame of size figures.
+    """
+    return df[[class_, feature]
+              ].groupby([class_, feature]
+              ).size(
+              ).rename("cnt"
+              ).to_frame(
+              ) / len(df)
+
+
+def get_class_cnts_by_features_nulls(df, class_, features):
+    """Retrieves, given a DataFrame, class column, and list
+    of feature column names, a sort of crosstab-vector,
+    where it's class col vs. all features, with a ratio
+    of True:False (in class) included as well.
+
+    Parameters
+    ----------
+
+    df : pandas.DataFrame
+        DataFrame on which this function will operate.
+
+    class_ : str
+	Column name for the class / target.
+
+    features : iterable
+	Iterable of column names of the feature(s).
+    
+    Return
+    ------
+
+    DataFrame with columns: [False, True, "tf_ratio], with
+    column names in `features`, broken out by null-or-not,
+    as indices.
+    """ 
+    groupbys = [df[[col, class_]
+                   ].groupby([col, class_]
+                   ).size(
+                   ).unstack(
+                   ).T.rename(columns={False: f"{col}_False",
+                                       True: f"{col}_True"})
+                for col in features]
+
+    groupbys[0].index.name = ""
+    _ahh = groupbys[0].join(groupbys[1:]).T.fillna(0)
+    _ahh["tf_ratio"] = _ahh[True] / _ahh[False]
+    return _ahh.sort_values("tf_ratio")
