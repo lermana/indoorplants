cbddf61e36c74dee334e4997e2453348dfffad1b
Abe Lerman <abe893@gmail.com>
2019-01-09 00:51:56 -0500

    Revert "Fix/3 (#18)" (#20)
    This reverts commit e8af41a1be629411176a442f7ad68677b9456598.

--- indoorplants/validation/crossvalidate.py
+++ indoorplants/validation/crossvalidate.py
@@ -5,64 +5,66 @@ from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.metrics import confusion_matrix
 
 
-def train_and_score(model_obj, score_funcs, X_train, y_train,
+def _train_and_score(model_obj, score_funcs, X_train, y_train,
                      X_test, y_test, train_scores=True):
     """Trains model to training data, outputs score(test data) 
     for each score in 'score_funcs', and does the same for 
     train data unless 'train_data' is set to False"""
     model = model_obj.fit(X_train, y_train)
-
     y_hat_train = model.predict(X_train)
     y_hat_test = model.predict(X_test)
-
-    return [(score_func(y_train, y_hat_train), score_func(y_test, y_hat_test))
-                if train_scores is True else
+    return [(score_func(y_train, 
+                        y_hat_train),
+            score_func(y_test, y_hat_test))
+            if train_scores is True else
             (score_func(y_test, y_hat_test))
-                for score_func in score_funcs]
+            for score_func in score_funcs]
 
 
-def cv_engine(X, y, model_obj, score_funcs, splits=5,
-              scale_obj=None, train_scores=True):
+def _cv_engine(X, y, model_obj, score_funcs, splits=5,
+               scale_obj=None, train_scores=True):
     """Splits data (based on whether model is classifier
     or regressor) and passes each fold to the train_and_score
-    function.
+    function. 
 
     Collects results and returns results as List."""
     if model_obj._estimator_type == 'classifier':
-        skf = StratifiedKFold(n_splits=splits, random_state=0)
-
+        skf = StratifiedKFold(n_splits=splits,
+                              random_state=0)
     elif model_obj._estimator_type == 'regressor':
-        skf = KFold(n_splits=splits, suffle=True, random_state=0)
-
+        skf = KFold(n_splits=splits,
+                    suffle=True,
+                    random_state=0)
     else:
         raise TypeError('Improper model type.')
 
     results = []
     for train, test in skf.split(X, y):
-        y_train = y.iloc[train]
-        y_test = y.iloc[test]
-
-        X_train = X.iloc[train, :]
-        X_test = X.iloc[test, :]
 
         if scale_obj is not None:
-            X_train = scale_obj.fit_transform(X_train)
-            X_test = scale_obj.fit_transform(X_test)
-
-        results.append(
-            train_and_score(model_obj, score_funcs, 
-                            X_train, y_train, X_test, y_test, 
-                            train_scores))
+            X_train = scale_obj.fit_transform(X.iloc[train, :])
+            X_test = scale_obj.fit_transform(X.iloc[test, :])
+        else:
+            X_train = X.iloc[train, :]
+            X_test = X.iloc[test, :]
+            
+        y_train = y.iloc[train]
+        y_test = y.iloc[test]
+        
+        results.append(_train_and_score(model_obj, score_funcs, 
+                                        X_train, y_train,
+                                        X_test, y_test, 
+                                        train_scores))
     return results
 
 
 def _cv_format(X, y, model_obj, score_funcs, splits=5,
                scale_obj=None, train_scores=True):
-    """Gets results from cv_engine and returns as 
+    """Gets results from _cv_engine and returns as 
     unaggregated DataFrame, where trial number & score 
     function used are represented in index."""
-    res = cv_engine(X, y, model_obj, score_funcs, 
-                    splits, scale_obj, train_scores)
+    res = _cv_engine(X, y, model_obj, score_funcs, 
+                     splits, scale_obj, train_scores)
     if train_scores is False:
         cols = [_.__name__ for _ in score_funcs]
         return pd.DataFrame(res, columns=cols)
@@ -79,7 +81,7 @@ def _cv_format(X, y, model_obj, score_funcs, splits=5,
         return dfs[0].join(dfs[1:])
 
 
-def describe_dataframe(results, stats_to_run):
+def _cv_score(results, stats_to_run):
     """
     Given DF of CV results, returns DF of descriptive statistics.
 
@@ -159,19 +161,21 @@ def cv_score(X, y, model_obj, score_funcs, splits=5,
     return
     ------
 
-    pandas.DataFrame of descriptive statistics, as specified in `describe_dataframe`.
+    pandas.DataFrame of descriptive statistics, as specified in `_cv_score`.
     """
     if callable(score_funcs):
         score_funcs = [score_funcs]
 
-    return describe_dataframe(
+    return _cv_score(
                 _cv_format(
-                    X, y, model_obj, score_funcs, splits, scale_obj, train_scores))
+                    X, y, model_obj, score_funcs, splits, scale_obj, train_scores
+                    )
+                )
 
 
 def cv_conf_mat(X, y, model_obj, splits=5, scale_obj=None):
     """Return confusion matrix for each CV trial."""
-    results = cv_engine(X=X, y=y, model_obj=model_obj, 
+    results = _cv_engine(X=X, y=y, model_obj=model_obj, 
                         score_funcs=[confusion_matrix], 
                         splits=splits, scale_obj=scale_obj, 
                         train_scores=False)
