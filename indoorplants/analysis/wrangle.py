6509b3fb5371ebca6429b4e08ee62bd0dbeaabf9
abe <alerman@contently.com>
2019-02-19 11:07:44 -0500

    adding feature distance functionality

--- indoorplants/analysis/wrangle.py
+++ indoorplants/analysis/wrangle.py
@@ -1,3 +1,8 @@
+import itertools
+import numpy as np
+import pandas as pd
+
+
 def get_feature_size_by_class(df, cls_col, features):
     """
     Given DataFrame, class column name, and feature column
@@ -183,3 +188,52 @@ def get_data_leak_cols_cls(df, cls_col,
         return list(set(set(missing_cols) | set(skewed_cols)))
     else:
         return {"missing": missing_cols, "skewed": skewed_cols}
+
+
+def euclidean(feature_one, feature_two):
+    return np.sqrt(
+                (feature_one - feature_two
+                   ).pow(2
+                   ).sum())
+
+
+
+def get_feature_iter_func(flat):
+    if flat:
+        return lambda feature_names: itertools.combinations(feature_names, 2)
+    else:
+        return lambda feature_names: itertools.product(feature_names, feature_names)
+
+
+def feature_distances(distance_func):
+
+    def inner(df, feature_names, flat=True):
+
+        iter_func = get_feature_iter_func(flat)
+        
+        scores = [(f1, f2, distance_func(df[f1], df[f2]))
+                  for f1, f2 in iter_func(feature_names)]
+        
+        scores_df = pd.DataFrame(scores, columns=["feature_one",
+                                                  "feature_two",
+                                                  f"{distance_func.__name__}"])
+        
+        if flat:
+            scores_df = scores_df.set_index(["feature_one",
+                                             "feature_two"]
+                                ).sort_values(f"{distance_func.__name__}", 
+                                              ascending=False)
+
+        else:
+            scores_df = scores_df.pivot(index='feature_one', 
+                                        columns='feature_two', 
+                                        values=f"{distance_func.__name__}")
+
+        return scores_df
+
+    return inner
+
+
+@feature_distances
+def euclidean_distances(feature_one, feature_two, flat=True):
+    return euclidean(feature_one, feature_two)
