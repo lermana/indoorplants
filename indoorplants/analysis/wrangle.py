55676d366846fc46db6ff7f288315f870298e5ed
Abe Lerman <abe893@gmail.com>
2019-02-19 15:15:51 -0500

    Fix/24 (#27)
    * saving work
    
    * saving work
    
    * fix setup.py issue:
    
    * slight reformatting in setup.py
    
    * adding feature distance functionality
    
    * updated feature distance functionality; adding normalize func

--- indoorplants/analysis/wrangle.py
+++ indoorplants/analysis/wrangle.py
@@ -1,3 +1,9 @@
+import itertools
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+
 def get_feature_size_by_class(df, cls_col, features):
     """
     Given DataFrame, class column name, and feature column
@@ -183,3 +189,72 @@ def get_data_leak_cols_cls(df, cls_col,
         return list(set(set(missing_cols) | set(skewed_cols)))
     else:
         return {"missing": missing_cols, "skewed": skewed_cols}
+
+
+def euclidean(feature_one, feature_two):
+    return np.sqrt(
+                (feature_one - feature_two
+                   ).pow(2
+                   ).sum())
+
+
+def euclidean_squared(feature_one, feature_two):
+    return (feature_one - feature_two
+               ).pow(2
+               ).sum()
+
+
+def get_feature_iter_func(flat):
+    if flat:
+        return lambda feature_names: itertools.combinations(feature_names, 2)
+    else:
+        return lambda feature_names: itertools.product(feature_names, feature_names)
+
+
+def normalize_min_max(df):
+    cols = df.columns
+    inds = df.index
+
+    arr = MinMaxScaler().fit_transform(df)
+    return pd.DataFrame(arr, columns=cols, index=inds)
+
+
+def feature_distances(distance_func):
+
+    def inner(df, feature_names_list, flat=True, normalize_data=True):
+        if normalize_data:
+            df = normalize_min_max(df[feature_names_list].astype(float))
+
+        iter_func = get_feature_iter_func(flat)
+
+        scores = [(f1, f2, distance_func(df[f1], df[f2]))
+                  for f1, f2 in iter_func(feature_names_list)]
+
+        scores_df = pd.DataFrame(scores, columns=["feature_one",
+                                                  "feature_two",
+                                                  f"{distance_func.__name__}"])
+
+        if flat:
+            scores_df = scores_df.set_index(["feature_one",
+                                             "feature_two"]
+                                ).sort_values(f"{distance_func.__name__}", 
+                                              ascending=False)
+
+        else:
+            scores_df = scores_df.pivot(index='feature_one', 
+                                        columns='feature_two', 
+                                        values=f"{distance_func.__name__}")
+
+        return scores_df
+
+    return inner
+
+
+@feature_distances
+def euclidean_distances(feature_one, feature_two, **kwargs):
+    return euclidean(feature_one, feature_two)
+
+
+@feature_distances
+def euclidean_squared_distances(feature_one, feature_two, **kwargs):
+    return euclidean_squared(feature_one, feature_two)
