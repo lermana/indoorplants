b7e91bde31a3bf5673e1ede8bdb979a4c7c94179
abe <alerman@contently.com>
2019-03-12 12:41:45 -0400

    git conflict resolved

--- indoorplants/analysis/wrangle.py
+++ indoorplants/analysis/wrangle.py
@@ -1,16 +1,10 @@
-import itertools
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-
-
 def get_feature_size_by_class(df, cls_col, features):
     """
     Given DataFrame, class column name, and feature column
     name, return:
     - pd.crosstab(df.cls_col, df.feature).stack()
     
-    Works differently if multiple features passed.
+    Works differently from `pd.crosstab` if multiple features passed.
 
     Parameters
     ----------
@@ -45,10 +39,13 @@ def get_feature_size_by_class(df, cls_col, features):
               ) / len(df)
 
 
-def get_class_cnts_by_features_nulls(df, class_, features):
-    """Retrieves, given a DataFrame, class column, and list
-    of feature column names, a sort of crosstab-vector,
-    where it's class col vs. all features, with a ratio
+def get_class_cnts_by_features_nulls(df, class_col, features):
+    """
+    Note: this function may be partially broken.
+
+    Retrieves, given: a DataFrame, class column, and list
+    of feature column names; a sort of crosstab-vector,
+    where it's `class_col` vs. all features, with a ratio
     of True:False (in class) included as well.
 
     Parameters
@@ -57,7 +54,7 @@ def get_class_cnts_by_features_nulls(df, class_, features):
     df : pandas.DataFrame
         DataFrame on which this function will operate.
 
-    class_ : str
+    class_col : str
     Column name for the class / target.
 
     features : iterable
@@ -70,8 +67,8 @@ def get_class_cnts_by_features_nulls(df, class_, features):
     column names in `features`, broken out by null-or-not,
     as indices.
     """ 
-    groupbys = [df[[col, class_]
-                   ].groupby([col, class_]
+    groupbys = [df[[col, class_col]
+                   ].groupby([col, class_col]
                    ).size(
                    ).unstack(
                    ).T.rename(columns={False: f"{col}_False",
@@ -82,6 +79,25 @@ def get_class_cnts_by_features_nulls(df, class_, features):
     _ahh = groupbys[0].join(groupbys[1:]).T.fillna(0)
     _ahh["tf_ratio"] = _ahh[True] / _ahh[False]
     return _ahh.sort_values("tf_ratio")
+
+def drop_users_nully_obj_cols(users):
+    """can this be generalized? does this kinda achieve what the above is trying and failing to do?"""
+    obj = users.select_dtypes(include=object)
+    obj_nulls = obj.isnull().sum()
+    obj = obj.join(users.is_spam)
+
+    obj_nice_table = obj.groupby("is_spam"
+                        ).count(
+                        ).T.join(obj_nulls.rename("null_cnt")
+                        ).sort_values("null_cnt", ascending=False
+                        ).join((obj_nulls[obj_nulls > 0] / len(users)
+                                ).rename("null_ratio"))
+
+    obj_nulls_90 = obj_nice_table[obj_nice_table.null_ratio >= .9].index
+
+    return users.drop(list(filter(lambda c: c not in ("blog_url", "portfolio_completed"),
+                                  obj_nulls_90)),
+                      axis=1)
 
 
 def get_null_stats(df):
@@ -167,72 +183,3 @@ def get_data_leak_cols_cls(df, cls_col,
         return list(set(set(missing_cols) | set(skewed_cols)))
     else:
         return {"missing": missing_cols, "skewed": skewed_cols}
-
-
-def euclidean(feature_one, feature_two):
-    return np.sqrt(
-                (feature_one - feature_two
-                   ).pow(2
-                   ).sum())
-
-
-def euclidean_squared(feature_one, feature_two):
-    return (feature_one - feature_two
-               ).pow(2
-               ).sum()
-
-
-def get_feature_iter_func(flat):
-    if flat:
-        return lambda feature_names: itertools.combinations(feature_names, 2)
-    else:
-        return lambda feature_names: itertools.product(feature_names, feature_names)
-
-
-def normalize_min_max(df):
-    cols = df.columns
-    inds = df.index
-
-    arr = MinMaxScaler().fit_transform(df)
-    return pd.DataFrame(arr, columns=cols, index=inds)
-
-
-def feature_distances(distance_func):
-
-    def inner(df, feature_names_list, flat=True, normalize_data=True):
-        if normalize_data:
-            df = normalize_min_max(df[feature_names_list].astype(float))
-
-        iter_func = get_feature_iter_func(flat)
-
-        scores = [(f1, f2, distance_func(df[f1], df[f2]))
-                  for f1, f2 in iter_func(feature_names_list)]
-
-        scores_df = pd.DataFrame(scores, columns=["feature_one",
-                                                  "feature_two",
-                                                  f"{distance_func.__name__}"])
-
-        if flat:
-            scores_df = scores_df.set_index(["feature_one",
-                                             "feature_two"]
-                                ).sort_values(f"{distance_func.__name__}", 
-                                              ascending=False)
-
-        else:
-            scores_df = scores_df.pivot(index='feature_one', 
-                                        columns='feature_two', 
-                                        values=f"{distance_func.__name__}")
-
-        return scores_df
-
-    return inner
-
-
-@feature_distances
-def euclidean_distances(feature_one, feature_two, **kwargs):
-    return euclidean(feature_one, feature_two)
-
-
-@feature_distances
-def euclidean_squared_distances(feature_one, feature_two, **kwargs):
-    return euclidean_squared(feature_one, feature_two)
