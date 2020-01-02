f02eda252084025ffa97254b0ec12c829c1e2651
Abe Lerman <abe893@gmail.com>
2020-01-02 15:55:18 -0500

    remove version tagging

--- .push.sh
+++ .push.sh
@@ -5,20 +5,14 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_repo() {
-  git add . *
-  git commit --message "Travis build: v${INDOORPLANTS_VERSION}"
-}
-
 upload_files() {
   if  git ls-remote --exit-code origin > /dev/null 2>&1; then
 	  git remote rm origin
   fi
 
   git remote add origin https://${GH_TOKEN}@github.com/lermana/indoorplants.git
-  git push --follow-tags origin master
+  git push origin master
 }
 
 setup_git
-commit_repo
 upload_files
